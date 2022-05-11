2d02b53b95362d7aacd8516241994e0172c04850
Abongile <abongilewilliam@gmail.com>
2022-05-11 22:06:04 +0200

    background change on btn working

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.querySelector('#btn');
+const background = document.querySelector('body'); 
+
+btn.addEventListener('click', ()=>{
+
+    let picked = colorPick();
+
+    background.style.backgroundColor = colors[picked];
+})
+
+//select a random color from the array 
+function colorPick(){
+    return Math.floor(Math.random() * colors.length);
+}
